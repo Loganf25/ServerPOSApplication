95173a8e1dbea28e9c145b5f5e9e0712f9476774
Lo Fl <lflora@cub.uca.edu>
2026-02-25 00:22:38 -0600

    Add project files.

---
+++ ServerPOSApplication/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServerPOSApplication.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
